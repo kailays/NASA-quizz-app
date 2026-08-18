9e4da872f3fc6b5ed4a520af95ebbd00a59b1d88
kailays <kailesh.mdu71@gmail.com>
2026-08-18 22:20:12 +0530

    Update Nasa quizz app.java

--- Nasa quizz app.java
+++ Nasa quizz app.java
@@ -1,3 +1,36 @@
 import java.util.scannwr;
 
 public class NasaQuizzApp {
+
+    public static void main(string[] args
+){
+
+          Scanner scanner = new Scanner(
+system.in);
+          int score = 0;
+
+          System.out.println("\nHi Everyone,
+This is a Nasa Quizz App.  Are you ready to test your knowledge and skills about the lovely subject space?"
+);
+
+          System.out.println("\nQuestion 1: What does NASA stand for?"); //
+First Question 
+          if(answer1.equalsIgnoreCase(
+"National aeronautics and space administration")) {
+             
+            system.out.println
+("Let's gooo! You got it right and your points are increased"
+);
+            score++;
+          }
+          else{
+               System.out.println("Sorry you got it wrong"
+);
+          }
+
+          system.out.println(
+"Your total score is:" + score);
+
+          System.out.println("\nQuestion 2: what is its natural satellite?"); //
+second question 
+        System.out
